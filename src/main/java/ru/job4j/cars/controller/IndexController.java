dc6b2c8d53ed5b20e45bb8c51e1a46e2454122aa
PavelM2205 <pavelm2205@mail.ru>
2023-05-01 10:44:39 +0700

    Добавлен вид index и класс IndexController

---
+++ src/main/java/ru/job4j/cars/controller/IndexController.java
@@ -0,0 +1,21 @@
+package ru.job4j.cars.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.cars.service.PostService;
+
+@Controller
+@AllArgsConstructor
+public class IndexController {
+    private final PostService postService;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("posts", postService.findAll());
+        return "index";
+    }
+
+
+}
